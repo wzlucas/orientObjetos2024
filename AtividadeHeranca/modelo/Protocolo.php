4b573eeac97042313d5e641f697bddd39ab46be1
lucas.bordini <131024143+wzlucas@users.noreply.github.com>
2024-11-20 23:31:55 -0300

    Add files via upload

---
+++ AtividadeHeranca/modelo/Protocolo.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once('Documento.php');
+require_once('Empenho.php');
+
+class Protocolo extends Documento {
+    private int $numeroLiquidacao;
+    private ?Empenho $empenho = null; 
+
+    public function getDadosDoc() {
+        if ($this->empenho !== null) {
+            return "Empenho: " . $this->empenho->getNumeroEmpenho() . "/" . $this->empenho->getAno() .  " (" . $this->numeroLiquidacao . ") - " . $this->getNome();
+        }
+        return "Empenho não associado.";
+    }
+
+    public function protocolar() {
+        return $this->getDadosDoc();
+    }
+
+
+    /**
+     * Get the value of numeroLiquidacao
+     */
+    public function getNumeroLiquidacao(): int
+    {
+        return $this->numeroLiquidacao;
+    }
+
+    /**
+     * Set the value of numeroLiquidacao
+     */
+    public function setNumeroLiquidacao(int $numeroLiquidacao): self
+    {
+        $this->numeroLiquidacao = $numeroLiquidacao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of empenho
+     */
+    public function getEmpenho(): Empenho
+    {
+        return $this->empenho;
+    }
+
+    /**
+     * Set the value of empenho
+     */
+    public function setEmpenho(Empenho $empenho): self
+    {
+        $this->empenho = $empenho;
+
+        return $this;
+    }
+}
